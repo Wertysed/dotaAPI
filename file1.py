3920a21c29205f6ca61402193888878f38edb11d
Wertysed <wertysed11@gmail.com>
2021-12-25 20:14:46 +0500

    some pasta

--- file1.py
+++ file1.py
@@ -144,7 +144,7 @@ async def player(ctx, argument):
 async def record(ctx, argument_1, argument_2, *, argument_3):
     argument_1 = int(argument_1)
     argument_2 = str(argument_2)
-    output_information = '\n'.join(DotaAPI.record_search(argument_1, argument_2, argument_3))
+    output_information = ' '.join(DotaAPI.record_search(argument_1, argument_2, argument_3))
     await ctx.send(output_information)
 
 
